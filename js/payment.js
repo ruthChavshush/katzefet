2cc5e8a01be2d57ed078a13e5e6c592594c1d270
ruthChavshush <xd.rchavshush@idf.il>
2024-03-06 23:20:36 +0200

    add payment page

---
+++ js/payment.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const submitBtn = document.getElementById("submitBtn")
+
+  submitBtn.addEventListener("click", function (event) {
+    event.preventDefault()
+    if ($("form")[0].checkValidity()) {
+      $("#myModal").modal("show")
+      $("form")[0].reset()
+    } else {
+      $("form")[0].reportValidity()
+    }
+  })
+})
